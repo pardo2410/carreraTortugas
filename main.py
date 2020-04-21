6252bbc5f10331a9549c84837dd14d3bff2c14f9
pardo2410 <pardo2410@gmail.com>
2020-04-21 21:16:09 +0200

    carrera de tortugas metodo competir

--- main.py
+++ main.py
@@ -2,9 +2,34 @@ import turtle
 
 class Circuito():
     corredores = []
+    __posStartY = (-30,-10,10,30)
+    __colorT= ('red','blue','pink','yellow')
+ 
+# Ajuste de la pantalla
     def __init__(self,width,height):
-        self.width = width
-        self.height = height
+#        self.width = width
+#        self.height = height
         
         self.__screen = turtle.Screen()
-    
+        self.__screen.setup(width, height)
+        self.__screen.bgcolor('green')
+        self.__starline = -width/2 + 20
+        self.__finishline = width/2 + 20
+        self.__createRunners()
+
+#caracteristicas de las tortugas
+    def __createRunners(self):
+        
+        for i in range(4):
+            new_turtle = turtle.Turtle()
+            new_turtle.color(self.__colorT[i])
+            new_turtle.shape('turtle')
+            new_turtle.penup()
+            new_turtle.setpos(self.__starline, self.__posStartY[i])
+                        
+            self.corredores.append(new_turtle)
+ 
+#funcion de carrera de tortugas 
+ 
+if __name__=='__main__':
+    circuito = Circuito(640, 480)
